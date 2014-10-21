521c0a9d5cb322a2b09e67cb4131e0e91b77411d
doping <doping@doping-N61Jq>
2014-10-21 12:45:37 +0100

    rovercircuit test

--- src/circuit/RoverCircuit.java
+++ src/circuit/RoverCircuit.java
@@ -12,20 +12,34 @@ public class RoverCircuit extends Individual {
 	private int size;
 	private int []circuit;
 	private ObservationData data;
-	private Double fit;
+	private Double fit = 0.0;
 	
 	//2 construtores
 	public RoverCircuit(ObservationData data){
 		this.data = data;
+		List<Integer> c = new ArrayList<Integer>();
+		for (int i = 0; i<data.getSize(); i++)
+			c.add(i);
+		
+		gen.setSeed(System.currentTimeMillis());
+		Collections.shuffle(c,gen);
+		for(int i:c)
+			circuit[i]=c.get(i);
+		
+		for(int i:circuit)
+			if (i>0)
+				fit += data.getCost(circuit[i-1], circuit[i]);
+			else
+				fit += data.getCost(circuit[circuit.length-1], circuit[i]);
+		
 		//gerar uma lista de inteiros cegamente (0..n-1)
 		//aplicar shuffle
-		//copiar para circuit
+		//copiar para Circuit
 	}
 
 	@Override
 	public double fitness() {
-		// TODO Auto-generated method stub
-		return 0;
+		return fit;
 	}
 
 	@Override
